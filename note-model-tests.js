ed3d53f3a3d2052ebbf3b19015a65b686d508e47
Andrew Wood <andrew.wood.dev@outlook.com>
2018-09-18 16:40:50 +0100

    tests for controller

--- note-model-tests.js
+++ note-model-tests.js
@@ -1,8 +1,9 @@
 
+// NOTES TEST
 firstNote = new Note("first note")
-
 assert.isEqual(firstNote.text(), "first note")
 
+// LIST TEST
 noteList = new NoteList()
 noteList.store(firstNote)
 noteList.createNote('secondNote')
@@ -10,17 +11,21 @@ noteList.createNote('secondNote')
 assert.isIncluded(noteList.notes, firstNote)
 assert.isEqual(noteList.notes[1].text(), 'secondNote')
 
+//VIEW TEST
 view = new View(noteList);
-
 assert.isEqual(view.html(), '<ul><li>first note</li><li>secondNote</li></ul>')
 
 emptyList = new NoteList()
 viewEmpty = new View(emptyList);
 assert.isEqual(viewEmpty.html(), '<ul></ul>')
 
+//CONTROLLER TEST
 controller = new NoteController(noteList);
 assert.isEqual(controller.constructor.name, "NoteController")
 
+controller.updateView()
+html = document.getElementById('app').innerHTML
+assert.isEqual(html, '<ul><li>first note</li><li>secondNote</li></ul>')
 
 
 assert.showLog()
