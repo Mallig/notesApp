e20aecea594fd5d5199527f0081538d4ff07718b
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-21 11:51:36 +0100

    test constructor has log, tests push result to log when run

--- vendors/rubber-duck/duck-assertions.js
+++ vendors/rubber-duck/duck-assertions.js
@@ -1,28 +1,46 @@
 (function(exports) {
 
   function Test() { 
-    this.log = new Log()
+    this.log = []
   }
 
   Test.prototype.isEqual = function(a, b) {
-    // this.log.store(a, b, 'equal')
-    return a === b;
+    if (a === b) {
+      this.log.push('PASS: ' + a + ' is equal to ' + b)
+    } else {
+      this.log.push('FAIL: ' + a + ' is not equal to ' + b)
+    }
   };
 
   Test.prototype.isTruthy = function(a) {
-    // this.log.store(a, 'truthy')
-    return Boolean(a) === true;
+    if (Boolean(a) === true) {
+      this.log.push('PASS: ' + a + ' is truthy')
+    } else {
+      this.log.push('FAIL: ' + a + ' is not truthy')
+    }
   };
 
   Test.prototype.isFalsy = function(a) {
-    // this.log.store(a, 'falsy')
-    return Boolean(a) === false;
+    if (Boolean(a) === false) {
+      this.log.push('PASS: ' + a + ' is falsy')
+    } else {
+      this.log.push('FAIL: ' + a + ' is not falsy')
+    }
   }
 
   Test.prototype.isIn = function(a, b) {
-    // this.log.store(a, b, 'in')
-    return a.includes(b);
+    if (a.includes(b)) {
+      this.log.push('PASS: ' + b + 'is included in ' + a)
+    } else {
+      this.log.push('FAIL: ' + b + 'is not included in ' + a)
+    }
   };
+
+  Test.prototype.viewLog = function() {
+    this.log.forEach(function(testResult) {
+      console.log(testResult)
+    })
+  }
   
   exports.Test = Test;
 
